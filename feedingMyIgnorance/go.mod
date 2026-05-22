2e35302c3b36877d0651c5561d0ce4656afb5e77
joyakubu <joelyakubu269@gmail.com>
2026-05-22 14:26:31 +0100

    converting string to art

---
+++ feedingMyIgnorance/go.mod
@@ -0,0 +1,3 @@
+module stringArt
+
+go 1.22.2
---
+++ feedingMyIgnorance/stringArt.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func StringToArt(input string) string {
+	if len(input) == 0 {
+		return ""
+	}
+	for _, r := range input {
+		if (r < '0' || r > '9') && r != '\n' {
+			return ""
+		}
+	}
+	m := map[rune][]string{
+		'0': {
+			" --- ",
+			" | | ",
+			" | | ",
+			" | | ",
+			" --- ",
+		},
+		'1': {
+			"  |  ",
+			"  |  ",
+			"  |  ",
+			"  |  ",
+			"  |  ",
+		},
+		'2': {
+			" --- ",
+			"    |",
+			" --- ",
+			"|    ",
+			" --- ",
+		},
+
+		'3': {
+			" --- ",
+			"    |",
+			" --- ",
+			"    |",
+			" --- ",
+		},
+
+		'4': {
+			"|  | ",
+			"|  | ",
+			" --- ",
+			"    |",
+			"    |",
+		},
+
+		'5': {
+			" --- ",
+			"|    ",
+			" --- ",
+			"    |",
+			" --- ",
+		},
+
+		'6': {
+			" --- ",
+			"|    ",
+			" --- ",
+			"|   |",
+			" --- ",
+		},
+
+		'7': {
+			" --- ",
+			"    |",
+			"   | ",
+			"  |  ",
+			" |   ",
+		},
+
+		'8': {
+			" --- ",
+			"|   |",
+			" --- ",
+			"|   |",
+			" --- ",
+		},
+
+		'9': {
+			" --- ",
+			"|   |",
+			" --- ",
+			"    |",
+			" --- ",
+		},
+	}
+	words := strings.Split(input, "\n")
+	var output string
+
+	for _, r := range words {
+		for i := 0; i < 5; i++ {
+			for _, c := range r {
+				if val, ok := m[c]; ok {
+					output += val[i]
+				}
+			}
+			output += "\n"
+
+		}
+	}
+	return output
+}
+func main() {
+	fmt.Print(StringToArt("12\n2"))
+}
